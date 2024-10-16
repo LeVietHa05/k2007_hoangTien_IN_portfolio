8cb50747d7db610fa27e6196438a65cf8544a1d7
LeVietHa05 <ha.leviet.0504@gmail.com>
2024-10-16 18:14:21 +0700

    updating sub pages

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.sendFile('views/index.html', { root: 'public' });
 });
+
+router.get('/piano', function (req, res, next) {
+  res.sendFile('views/piano.html', { root: 'public' });
+})
+router.get('/music', function (req, res, next) {
+  res.sendFile('views/music.html', { root: 'public' });
+})
+router.get('/drama', function (req, res, next) {
+  res.sendFile('views/drama.html', { root: 'public' });
+})
+router.get('/writing', function (req, res, next) {
+  res.sendFile('views/writing.html', { root: 'public' });
+})
 
 module.exports = router;
