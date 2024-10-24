6ac86b5e05874a3cb06364506f688fd12ebc6769
LeVietHa05 <ha.leviet.0504@gmail.com>
2024-10-24 17:09:49 +0700

    done about

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,11 @@ router.get('/', function (req, res, next) {
   res.sendFile('views/index.html', { root: 'public' });
 });
 
-router.get('/piano', function (req, res, next) {
-  res.sendFile('views/piano.html', { root: 'public' });
+router.get('/about', function (req, res, next) {
+  res.sendFile('views/about.html', { root: 'public' });
 })
 router.get('/music', function (req, res, next) {
   res.sendFile('views/music.html', { root: 'public' });
-})
-router.get('/drama', function (req, res, next) {
-  res.sendFile('views/drama.html', { root: 'public' });
 })
 router.get('/writing', function (req, res, next) {
   res.sendFile('views/writing.html', { root: 'public' });
